c729dda19d0ef5da1606e5b4768adf3797d69784
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-05-02 14:47:11 +0200

    Creazione componente "AppTitle" e stampa a schermo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
+  import AppTitle from "./components/AppTitle.vue"
+
+  export default{
+      name: 'App',
+      components:{
+        AppTitle
+      }
+    }
 </script>
 
 <template>
-  
+  <div id="container0" class="vh-100 d-flex justify-content-center align-items-center">
+    <AppTitle/>  
+  </div>
 </template>
 
 <style scoped>
-
+  #container0{
+    background-color: beige;
+  }
 </style>
 
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,10 +1,22 @@
 <script>
- 
+  export default{
+    name: 'AppTitle',
+    data(){
+      return{
+        title: 'La mia prima app con Vite!'
+      }
+    }
+  }
 </script>
 
 <template>
-  
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
 </template>
 
 <style scoped>
+  h1{
+    color: red
+  }
 </style>
